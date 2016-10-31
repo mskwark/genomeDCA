9421ad4b9b196e6ac95d3f1c4328cc8dc94bf09f
Marcin J. Skwark <marcin@skwark.pl>
2016-10-31 01:40:29 -0500

    Updated method description

---
+++ 02inference/00rungenomeDCA.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import sys, subprocess, os
+import string as s
+
+# Change 4 to the desired number of cores genomeDCA is to use.
+cores = 4
+
+### Change this to reflect the path to your MATLAB executable
+matlab = '/change/this/path'
+####
+
+### Change this to reflect the path to the genomeDCA code
+plmdca = '/home/guest/software/genomeDCA'
+####
+
+try:
+    if len(sys.argv) < 2:
+        raise
+    try:
+        if not os.path.exists(matlab):
+            sys.stderr.write('MATLAB executable path is not set properly. Trying to find it in system path\n')
+            matlab = subprocess.check_output(['which', 'matlab'])
+    except:
+        sys.stderr.write('Cannot find MATLAB executable, the path in {:s} is set erroneusly and the executable cannot be found automatically.\n'.format(sys.argv[0]))
+        sys.exit(0)
+        
+    if not os.path.exists(plmdca + '/plmDCA_asymmetric.m'):
+        sys.stderr.write('genomeDCA path is not set properly. Trying to find it in {:s}\n'.format(os.path.dirname(os.path.realpath(sys.argv[0]))))
+
+
+    for infile in sys.argv[1:]:
+        os.chdir(os.path.abspath(infile)[:os.path.abspath(infile).rfind('/')]) 
+        infilestem = infile.split('/')[-1]
+        infilestem = infilestem[:infilestem.rfind('.')]
+
+        if not os.path.exists(infilestem + ".genomedca"):
+            print "Running genomeDCA on {:s}'".format(infile)
+            t = check_output([matlab, '-nodesktop', '-r', "path(path, 'PLMDCAPATH'); path(path, 'PLMDCAPATH/functions'); path(path, 'PLMDCAPATH/3rd_party_code/minFunc/'); plmDCA_asymmetric ( '".replace('PLMDCAPATH', plmdca) + infilestem + ".seq', '" + infilestem + ".genomedca', 0.1, {:d}); exit".format(cores)])
+except:
+    sys.stderr.write('{:s} inputfile1.seq [inputfile2.seq ...]\n'.format(sys.argv[0])) 
+    sys.stderr.write('\nThis script will run genomeDCA on all the input files provided in the command line\nREMEMBER: set the path to MATLAB executable and genomeDCA code in the header of the script\n')
---
+++ 02inference/README.md
@@ -0,0 +1,23 @@
+# Coupling inference
+
+This step will infer evolutionary couplings between loci in pseudo-alignments generated in the previous step. These steps may be time consuming and therefore, it is strongly suggested that large scale computational resources are to be used.
+
+## Prerequisite:
+* sampled alignments generated in previous step (directory sampled/)
+* MATLAB
+* modified plmDCA (included in ./genomeDCA)
+
+## Procedure
+1. (before first use) Edit 00rungenomeDCA.py
+* Set the path to your MATLAB executable
+* Set the desired number of cores to use, or use -1 for the program to use all available cores
+* Set the absolute path to the ./genomeDCA directory
+
+2. For each of the alignments, run ./00rungenomeDCA.py. Preferably on a computational cluster.
+
+Example:
+`$> for t in sampled/sampling*/input.seq; do ./00rungenomeDCA.py $t; done`
+
+Note: `./00rungenomeDCA` can take more than one alignment input in the command line. In such a case, the inference will be run sequentially.
+
+3. Wait until inference finishes -- may take a considerable amount of time.
