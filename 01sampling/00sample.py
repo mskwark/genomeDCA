72dfeaaec28b486bddbdfe1f6d545395364d60ca
Marcin J. Skwark <marcin@skwark.pl>
2016-10-31 01:13:18 -0500

    Updated method descriptions

--- 01sampling/00sample.py
+++ 01sampling/00sample.py
@@ -5,59 +5,74 @@ import numpy as np
 import random
 import joblib
 
+## Adjustable parameters
+stepSize = 1500
+samples = 5000
 
-accepted = []
+def generateSamples(datafile, stepSize=1500, samples=5000):
+    accepted = []
+    print 'Read data for sampling'
+    data = joblib.load(datafile)
 
-# Uses processed.dat from previous step (not included here)
+    accepted = sorted(data[data.keys()[0]].keys())
+    chunks = []
+    start = 0
+    chunk = []
 
-print 'Read data for sampling'
-#data = joblib.load('processed.dat')
-data = joblib.load('/hd0/skwarkmj/maela/processed.dat')
+    # Produce the bins from which to sample
+    for i in accepted:
+        if i > start + stepSize:
+            start += stepSize
+            if len(chunk) > 0:
+                chunks.append(chunk)
+        chunk.append(i)
+        
+    for xxv in range(samples):
+        accepted2 = []
 
-accepted = sorted(data[data.keys()[0]].keys())
+        # Pick the entries from the bins
+        for i in range(0, len(chunks)):
+            accepted2.append(random.choice(chunks[i]))
 
-step = 1500
+        # Save the alignment of sampled bins and mapping of positions to
+        # original loci
+        i = 0
+        while os.path.exists('sampled/sampling{0:04d}'.format(i)):
+            i+=1
+        os.makedirs('sampled/sampling{0:04d}'.format(i))
 
-chunks = []
-start = 0
-chunk = []
+        f = open('sampled/sampling{0:04d}/input.seq'.format(i), 'w')
+        print i, len(accepted2)
+        for sequence in data.keys():
+            S = data[sequence]
+            f.write('>{:s}\n'.format(sequence))
+            for ii in accepted2:
+                f.write(data[sequence][ii])
+            f.write('\n')
+        f.close()
 
-# Produce the bins from which to sample
-for i in accepted:
-    if i > start + step:
-        start += step
-        if len(chunk) > 0:
-            chunks.append(chunk)
-            print start, len(chunk), min(chunk), max(chunk)
-    chunk.append(i)
-    
-for xxv in range(5000):
-    accepted2 = []
+        s = 0
+        f = open('sampled/sampling{0:04d}/input.mapping'.format(i), 'w')
+        for i in accepted2:
+            f.write('{0:d} {1:d}\n'.format(s, i))
+            s+=1
+        f.close()
 
-    # Pick the entries from the bins
-    for i in range(0, len(chunks)):
-        accepted2.append(random.choice(chunks[i]))
 
-    # Save the alignment of sampled bins and mapping of positions to
-    # original loci
-    i = 0
-    while os.path.exists('sampled/samplingU{0:04d}'.format(i)):
-        i+=1
-    os.makedirs('sampled/samplingU{0:04d}'.format(i))
-
-    f = open('sampled/samplingU{0:04d}/maela.seq'.format(i), 'w')
-    print i, len(accepted2)
-    for sequence in data.keys():
-        S = data[sequence]
-        f.write('>{:s}\n'.format(sequence))
-        for ii in accepted2:
-            f.write(data[sequence][ii])
-        f.write('\n')
-    f.close()
-
-    s = 0
-    f = open('sampled/samplingU{:04d}/maela.mapping'.format(i), 'w')
-    for i in accepted2:
-        f.write('{0:d} {1:d}\n'.format(s, i))
-        s+=1
-    f.close()
+if __name__ == "__main__":
+    try:
+        infile = sys.argv[1]
+        if not os.path.exists(infile):
+            sys.stderr.write('The input file ({:s}) does not exist!\n'.format(infile))
+            sys.exit(2)
+        if len(sys.argv) == 2:
+            generateSamples(infile)
+        elif len(sys.argv) == 4:
+            generateSamples(infile, stepSize=float(sys.argv[2]), samples=int(sys.argv[3]))
+        else:
+            raise 
+    except Exception as e:
+        sys.stderr.write('Syntax: {:s} inputFile\n'.format(sys.argv[0]))
+        sys.stderr.write('        {:s} inputFile stepSize samplesNumer\n'.format(sys.argv[0]))
+        sys.stderr.write(' If step size and number of samples are not provided, they will be given default values of 1,500 bp and 5000 samples.\n'.format(sys.argv[0]))
+        sys.exit(2)
---
+++ 01sampling/README.md
@@ -0,0 +1,15 @@
+# Sampling
+
+This step generates randomized pseudo-alignments used in the subsequent steps. Each of the pseudo-alignments has a fixed width, roughly equal to the length of full genome sequence divided by the step size. Randomized sampling enables computational inference of the the couplings within attainable memory limits.
+
+The only prerequisite for this step is a data file (pickled dictionary) produced in the previous step. As a result of running this script a series of directories named `sampled/samplingXXXX` (where X's stand for digits) will be produced. If a particular directory exists, it will be omitted, not overwritten. The script can be run multiple times to yield more data for analysis.
+
+## Adjustable parameters
+        * The number of samples to produce (change 5000 to desired number). The more samples, the greater expected accuracy of the method.
+        * Step size (currently 1,500 bp). The greater the step size, potentially 
+            more unbiased the inference, at expense of missing some medium-range
+            interactions and necessity of running more iterations (samples). Smaller step sizes increase the memory requirements of the inference step
+
+## Procedure
+Run 00sample.py (for internal operation details, see source code)
+`./00sample.py myGenomes.dat`.
